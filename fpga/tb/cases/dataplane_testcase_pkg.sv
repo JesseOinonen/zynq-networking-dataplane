04bcb76557d23f5d516fbffc03c4e5691150ef7b
Jesse Oinonen <jesse.oinonen25@gmail.com>
2026-01-21 14:04:16 +0200

    tb for axi update

--- fpga/tb/cases/dataplane_testcase_pkg.sv
+++ fpga/tb/cases/dataplane_testcase_pkg.sv
@@ -2,12 +2,22 @@
 package dataplane_testcase_pkg;
     `include "../top/register.svh"
 
-    task dataplane_testcase(input axi_lite_if axi, input logic clk, input logic rst_n);
+    task dataplane_axi4_lite_testcase(input virtual axi_lite_if axi, input logic clk, input logic rst_n);
+        logic [31:0] read_data;
+        
         @(posedge rst_n);
-        $display("Running dataplane_testcase...");
-        axi.write(ADDR, DATA);
-        repeat (500) @(posedge clk);
-        $display("Completed dataplane_testcase.");
+        $display("Running dataplane_axi4_lite_testcase...");
+        @(posedge clk);
+        axi.write(32'hffffffff, 32'hAAAAAAAA);
+        repeat (10) @(posedge clk);
+        axi.read(32'hffffffff, read_data);
+        if (read_data !== 32'hAAAAAAAA) begin
+            $error("Data mismatch: expected 0xAAAAAAAA, got 0x%08X", read_data);
+        end 
+        else begin
+            $display("Data match successful: 0x%08X", read_data);
+        end
+        $display("Completed dataplane_axi4_lite_testcase.");
     endtask
 
 endpackage
--- fpga/tb/top/axi_lite_if.sv
+++ fpga/tb/top/axi_lite_if.sv
@@ -1,34 +1,28 @@
-interface axi_lite_if(input logic clk);
-    logic [31:0] AWADDR, WDATA, ARADDR, RDATA;
-    logic        AWVALID, WVALID, ARVALID, RREADY, BREADY;
-    logic        AWREADY, WREADY, ARREADY, RVALID, BVALID;
-    logic [3:0]  WSTRB;
-    logic [1:0]  BRESP, RRESP;
+task automatic write(input logic [31:0] addr, input logic [31:0] data);
+    AWADDR  = addr;
+    WDATA   = data;
+    AWVALID = 1; 
+    WVALID = 1; 
+    WSTRB = 4'b1111;
+    wait (AWREADY && WREADY);
+    @(posedge clk) 
+    AWVALID = 0; 
+    WVALID = 0;
+    BREADY = 1; 
+    wait(BVALID); 
+    @(posedge clk) 
+    BREADY = 0;
+endtask
 
-    // Write task
-    task automatic write(input logic [31:0] addr, input logic [31:0] data);
-        AWADDR  = addr;
-        WDATA   = data;
-        AWVALID = 1; 
-        WVALID = 1; 
-        WSTRB = 4'b1111;
-        wait (AWREADY && WREADY);
-        AWVALID = 0; 
-        WVALID = 0;
-        BREADY = 1; 
-        wait(BVALID); 
-        BREADY = 0;
-    endtask
-
-    // Read task
-    task automatic read(input logic [31:0] addr, output logic [31:0] data);
-        ARADDR = addr; 
-        ARVALID = 1; 
-        wait(ARREADY); 
-        ARVALID = 0;
-        RREADY = 1; 
-        wait(RVALID); 
-        data = RDATA; 
-        RREADY = 0;
-    endtask
-endinterface
+task automatic read(input logic [31:0] addr, output logic [31:0] data);
+    ARADDR = addr; 
+    ARVALID = 1; 
+    wait(ARREADY); 
+    @(posedge clk) 
+    ARVALID = 0;
+    RREADY = 1; 
+    wait(RVALID); 
+    @(posedge clk) 
+    data = RDATA; 
+    RREADY = 0;
+endtask
