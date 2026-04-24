3c1af5265061b9f21d0bf6f8f2b631f55cc2ddc6
Jesse <jesse.oinonen25@gmail.com>
2026-04-24 14:51:49 +0300

    uvm options for tcl  based simulation

--- vivado/scripts/sim.tcl
+++ vivado/scripts/sim.tcl
@@ -1,6 +1,9 @@
 open_project ./dataplane.xpr
 set_property top top [get_filesets sim_1]
 
+set_property -name {xsim.compile.xvlog.more_options} -value {-L uvm} -objects [get_filesets sim_1]
+set_property -name {xsim.elaborate.xelab.more_options} -value {-L uvm} -objects [get_filesets sim_1]
+
 puts "======================================"
 puts "Launching Vivado Simulation GUI"
 puts "======================================"
